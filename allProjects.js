57b053a4829012114b0358e53fd5f594e5a80733
Deniz <auo@tuta.io>
2025-11-23 19:48:38 +0100

    rendered selected project on UI.

--- allProjects.js
+++ allProjects.js
@@ -1,34 +1,42 @@
- import { latestData, featuredData, olderData } from "./data.js";
-
-        
-        const allProjectsArray = [...latestData, ...featuredData, ...olderData]
-        const allTagsArray = allProjectsArray
-                                    .map(project => project.tags)
-                                    .flat()                          
-        const reducedTags = [...new Set(allTagsArray)]
-
-        const rootDiv = document.getElementById("root")
-
-        const selectBox = document.createElement("select")
-
-        reducedTags.forEach(tag => {
-            const option = document.createElement("option")
-            option.value = tag
-            option.textContent = tag
-
-            selectBox.append(option)
-        })
-        const selectedValuePara = document.createElement("p")
-
-        rootDiv.append(selectBox, selectedValuePara)
-
-        selectBox.addEventListener("change", () => {
-
-            console.log(selectBox.value)
-
-            selectedValuePara.textContent = selectBox.value
+import { latestData, featuredData, olderData } from "./data.js";
+import { renderCarousel } from "./carousel.js";
 
 
+const allProjectsArray = [...latestData, ...featuredData, ...olderData]
+const allTagsArray = allProjectsArray
+    .map(project => project.tags)
+    .flat()
+const reducedTags = [...new Set(allTagsArray)]
+const rootDiv = document.getElementById("root")
+const selectBox = document.createElement("select")
+selectBox.id = "project-filter"
 
-        })
+reducedTags.forEach(tag => {
+
+    const option = document.createElement("option")
+    option.value = tag
+    option.textContent = tag
+
+    selectBox.append(option)
+
+})
+const selectedValuePara = document.createElement("p")
+
+rootDiv.append(selectBox, selectedValuePara)
+
+selectBox.addEventListener("change", () => {
+    selectedValuePara.textContent = selectBox.value
+    // console.log(allProjectsArray)
+
+    const selectedProjectsArray = allProjectsArray.filter(project => {
+        // console.log(project.tags.includes(selectBox.value))
+        return project.tags.includes(selectBox.value)
+    
+    
+    })
+
+    renderCarousel(selectedProjectsArray, "selected-carousel")
+
+    console.log(selectedProjectsArray)
+})
 
