e549489cc326e2ea1fac80f750a91f3d2cb40918
Deniz <auo@tuta.io>
2025-11-24 14:45:59 +0100

    minor changes

--- data.js
+++ data.js
@@ -97,7 +97,7 @@ export const olderData = [
     img: "./media/movietimelist.jpg",
     alt: "Image of an app to search movies.",
     title: "Movie Search",
-    tags: ["JavaScript", "API"],
+    tags: ["JavaScript", "API", "Grid"],
     appLink: "https://movietimelist.netlify.app/",
     codeLink: "https://github.com/zenidreney/movie-watchlist"
   },
